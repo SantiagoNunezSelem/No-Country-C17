cfdfe4bce0296d7bd16e3768f15076b0a21210ca
Santiago N.S <gigogisantisd@gmail.com>
2024-04-02 13:14:49 -0300

    Agregar el componente navbar

--- front/src/pages/_document.js
+++ front/src/pages/_document.js
@@ -1,10 +1,14 @@
 import { Html, Head, Main, NextScript } from "next/document";
+import NavBar from "../../components/NavBar.js"
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head />
       <body>
+        <header>
+          <NavBar></NavBar>
+        </header>
         <Main />
         <NextScript />
       </body>
