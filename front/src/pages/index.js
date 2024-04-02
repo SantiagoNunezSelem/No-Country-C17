fa4198ba0d6b8d9c9af591b02c90db492b3c1b00
Santiago N.S <gigogisantisd@gmail.com>
2024-04-02 16:25:05 -0300

    navBar y foto en home (maqueta)

--- front/src/pages/index.js
+++ front/src/pages/index.js
@@ -1,14 +1,30 @@
 import Image from "next/image";
 import { Inter } from "next/font/google";
+import NavBar from "@/components/NavBar";
+
+import img from "../img/carrusel-1.jpg"
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
     return (
-      <main
-        className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-      >
+      <main className="w-full">
+
+        <div className="background-decoration-grey"></div>
+
+        <section id="home-section">
+          <NavBar></NavBar>
+          <div className="flex justify-center items-center  w-full h-full">
+            <Image id="img1" src={img}></Image>
+          </div>
+        </section>
+
         
+
+        <section className="flex justify-center w-full h-screen">
+          Otra seccion
+        </section>
+
 
       </main>
     );
