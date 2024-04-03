2a6f63217946828aa6ae1345d19b1d34be7d6acf
Santiago N.S <gigogisantisd@gmail.com>
2024-04-02 23:05:56 -0300

    Update index.js

--- front/src/pages/index.js
+++ front/src/pages/index.js
@@ -1,30 +1,20 @@
 import Image from "next/image";
 import { Inter } from "next/font/google";
-import NavBar from "@/components/NavBar";
-
-import img from "../img/carrusel-1.jpg"
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
     return (
       <main className="w-full">
-
-        <div className="background-decoration-grey"></div>
-
-        <section id="home-section">
-          <NavBar></NavBar>
-          <div className="flex justify-center items-center  w-full h-full">
-            <Image id="img1" src={img}></Image>
-          </div>
-        </section>
-
+      
+        <div id="home-img">
+          <div id="gradient-home"></div>
+        </div>
         
 
-        <section className="flex justify-center w-full h-screen">
-          Otra seccion
+        <section className="flex justify-center w-full">
+          <div>Otra seccion</div>
         </section>
-
 
       </main>
     );
