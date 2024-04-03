641a727b11460c1ccf2a1467450f79ce36d31f5d
Santiago N.S <gigogisantisd@gmail.com>
2024-04-02 23:05:20 -0300

    Update NavBar.js

--- front/src/components/NavBar.js
+++ front/src/components/NavBar.js
@@ -6,11 +6,13 @@ function NavBar() {
     <nav className='flex flex-row w-full py-5'>
         <div className='flex items-center ml-5'>LOGO</div>
         <div className='flex justify-end w-full'>
-            <ul className='flex flex-row space-x-4 pr-5 pl-2 py-2 border-b-2'>
-                <Link href="#">SERVICIOS</Link>
-                <Link href="#">UBICACIÓN</Link>
-                <Link href="#">NOSOTROS</Link>
-                <Link href="#">ASESORÍA</Link>
+            <ul className='flex flex-row space-x-10 pr-20 pl-2 py-10'>
+                <Link href="#">Inicio</Link>
+                <Link href="#">Servicios</Link>
+                <Link href="#">Sucursales</Link>
+                <Link href="#">Staff</Link>
+                <Link href="#">Conctacto</Link>
+                <Link href="#">Reservar una cita</Link>
             </ul>
         </div>
         
