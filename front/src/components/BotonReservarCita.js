490175202ac7992632d3a58491ee0185d4ba2cdc
Santiago N.S <gigogisantisd@gmail.com>
2024-04-05 13:08:32 -0300

    cambio de img

--- front/src/components/BotonReservarCita.js
+++ front/src/components/BotonReservarCita.js
@@ -1,14 +1,15 @@
 import React from 'react'
+import Image from 'next/image'
+import calendarIcon from "@/img/calendar-icon.png"
 
 function BotonReservarCita( {url} ) {
     return (
     <a href="#" className="w-64 mb-5 flex align-center justify-center self-center text-center rounded-md px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600" style={{backgroundColor:"#F84646"}}>
-        {/* icon */}
-        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
-            <path strokeLinecap="round" strokeLinejoin="round" d="M6.75 3v2.25M17.25 3v2.25M3 18.75V7.5a2.25 2.25 0 0 1 2.25-2.25h13.5A2.25 2.25 0 0 1 21 7.5v11.25m-18 0A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75m-18 0v-7.5A2.25 2.25 0 0 1 5.25 9h13.5A2.25 2.25 0 0 1 21 11.25v7.5" />
-        </svg>
+        
+        <Image src={calendarIcon} width={30} height={"auto"}/>
+        
+        <p className="my-auto ml-5 text-lg">Reservar cita</p>
 
-        <p className="ml-5">Reservar cita</p>
     </a>
     )
 }
