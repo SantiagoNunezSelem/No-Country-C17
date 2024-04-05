927bd308fe68e0389424ee844bc30cb20289b6ab
Santiago N.S <gigogisantisd@gmail.com>
2024-04-05 13:09:51 -0300

    borrar referencias de diseño

--- front/src/pages/index.js
+++ front/src/pages/index.js
@@ -11,22 +11,10 @@ export default function Home() {
           <h1 id="home-h1">TU BARBERIA<br/>PROFESIONAL</h1>
         </div>
 
-        {/* Borrar esta seccion despues */}
-        <section id="prueba" className="flex justify-center w-full bg-slate-700">
-          <div>Otra seccion</div>
-        </section>
-        {/*  */}
-
         <section className="flex flex-col justify-center w-full" style={{backgroundColor:"#292929"}}>
           <h1 className="section-title">SUCURSALES</h1>
           <Sucursales/>
         </section>
-
-        {/* Borrar esta seccion despues */}
-        <section id="prueba" className="flex justify-center w-full bg-slate-700">
-          <div>Otra seccion</div>
-        </section>
-        {/*  */}
 
       </main>
     );
