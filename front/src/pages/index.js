d71e7335ae4fa115985a702efae5f2f8c5f51e10
Santiago N.S <gigogisantisd@gmail.com>
2024-04-05 12:28:38 -0300

    maqueta seccion sucursales de index-page

--- front/src/pages/index.js
+++ front/src/pages/index.js
@@ -1,4 +1,5 @@
 import { Inter } from "next/font/google";
+import Sucursales from "@/components/SucursalesContainer"
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -9,10 +10,23 @@ export default function Home() {
         <div id="home-img">
           <h1 id="home-h1">TU BARBERIA<br/>PROFESIONAL</h1>
         </div>
-        
-        <section className="flex justify-center w-full">
+
+        {/* Borrar esta seccion despues */}
+        <section id="prueba" className="flex justify-center w-full bg-slate-700">
           <div>Otra seccion</div>
         </section>
+        {/*  */}
+
+        <section className="flex flex-col justify-center w-full" style={{backgroundColor:"#292929"}}>
+          <h1 className="section-title">SUCURSALES</h1>
+          <Sucursales/>
+        </section>
+
+        {/* Borrar esta seccion despues */}
+        <section id="prueba" className="flex justify-center w-full bg-slate-700">
+          <div>Otra seccion</div>
+        </section>
+        {/*  */}
 
       </main>
     );
