f5e7e197ca457f2ebbf957a14db68a1906909b8a
Santiago N.S <gigogisantisd@gmail.com>
2024-04-08 14:14:10 -0300

    Footer

--- front/src/pages/_document.js
+++ front/src/pages/_document.js
@@ -1,6 +1,6 @@
 import { Html, Head, Main, NextScript } from "next/document";
 import NavBar from "@/components/NavBar";
-
+import Footer from "@/components/Footer";
 export default function Document() {
   return (
     <Html lang="en">
@@ -16,6 +16,7 @@ export default function Document() {
         </header>
         <Main />
         <NextScript />
+        <footer><Footer></Footer></footer>
       </body>
     </Html>
   );
