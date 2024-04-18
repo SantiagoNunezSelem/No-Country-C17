d0ec06f944d1972766fe814544c582187025878b
MrM-10 <marianomessina10@hotmail.com>
2024-04-18 09:33:33 -0300

    continuo trabajo comp ResumenServicio

--- front/src/components/sitemaCitas/ResumenServicio.js
+++ front/src/components/sitemaCitas/ResumenServicio.js
@@ -2,21 +2,57 @@ import { Container } from 'postcss';
 import React from 'react';
 import { Box, IconButton, Typography, Grid } from "@mui/material";
 import ServiciosReservados from '../ServicePickerInfo';
+import { Height } from '@mui/icons-material';
+import { Koulen } from 'next/font/google';
 
 let tarjetaResumenReserva= ServiciosReservados()
+
+const datosUsuarioMock=[
+    {
+    nombre:'usuario',
+    apellido:'nuevo',
+    email:'email@web',
+    telefono:'+541234567890',
+    comentario:'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'
+    }
+]
+
 function ResumenServicio(){
+
     return(
         <div>
-            <Typography id='titulo-resumen-reserva' justifyContent='center' alignContent="center" variant='subtitle1' sx={{color:"#F84646"}}>Tus Servicios Reservados</Typography>
-            <Box  sx={{
-                display:'flex',
-                flexDirection: 'column',
+            <Typography variant="h1" justifyContent='center' alignContent="center" variant='subtitle1' 
+            sx={{
+                width:"fit-content",
+                p: 1,
+                color:"#F84646"
                 }}
-                >
-                <div>
-                    {tarjetaResumenReserva}
-                </div>
-            </Box>
+                >Tus Servicios Reservados</Typography>
+            <Grid gridTemplateColumns={2}>
+                <Box  sx={{
+                    gridColumn:1,
+                    display:'flex',
+                    flexDirection: 'row',
+                    margin: 1,
+                    }}
+                    >
+                    <div>
+                        {tarjetaResumenReserva}
+                    </div>
+                </Box>
+                <Box  sx={{
+                    gridColumn:2,
+                    display:'flex',
+                    flexDirection: 'row',
+                    margin: 1,
+                    }}
+                    >
+                    <div>
+                        Datos Usuario
+                        {datosUsuarioMock.nombre}
+                    </div>
+                </Box>
+            </Grid>
         </div>
         );
 }
