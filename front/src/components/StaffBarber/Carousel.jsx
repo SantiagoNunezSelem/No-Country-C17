1d0b58abc85111dbe00fadbdb4c935bf798e4a26
damian sola <damiansola99@gmail.com>
2024-04-17 21:17:36 -0300

    la seccion staff funciona perfectamente

--- front/src/components/StaffBarber/Carousel.jsx
+++ front/src/components/StaffBarber/Carousel.jsx
@@ -32,14 +32,14 @@ function Carousel({data}) {
   // data = data.slice(0,4)
 
   return (
-    <div className="w-5/6 p-6 m-auto text-center items-center justify-center">
+    <div className="w-5/6 p-6 m-auto  justify-center">
       <Slider {...settings}>
         {data ? data.map(e => {
-          return <div key={e.idEmpleado} className="w-64 h-64">
-            <div className="w-56 h-56 bg-white flex items-center justify-center rounded-full">
+          return <div key={e.idEmpleado} className="text-center block w-56 h-56">
+            <div className="w-56 m-auto h-56 bg-white flex items-center justify-center rounded-full">
               <img src={e.imagen} alt={e.nombre} className=" rounded-full object-contain h-full w-full "/>
             </div>
-              <p className="text-white p-2 text-3xl font-medium m-auto">{e.nombre.toUpperCase()}</p>
+              <p className="text-white p-2 text-3xl font-medium ">{e.nombre.toUpperCase()}</p>
           </div>
         }): <img src="https://media1.giphy.com/media/uIJBFZoOaifHf52MER/200w.gif?cid=6c09b952f5fbka6mi8iznnb5x3u3y3otftzrb5rxyl8f08lf&ep=v1_gifs_search&rid=200w.gif&ct=g"/>}
       </Slider>
--- front/src/components/StaffBarber/index.jsx
+++ front/src/components/StaffBarber/index.jsx
@@ -21,12 +21,18 @@ const getSucursal = async() => {
 
 const StaffBarber = () => {
 
+
     const [barbers, setBarbers] = useState([]);
+    const [show, setShow] = useState([])
     const [sucursal,  setSucursal] = useState([]);
+    
+    console.log(barbers)
+    
 
     let getBarb = async () => {
         let list = await getBarbers()
         setBarbers(list.data.rows)
+        setShow(list.data.rows)
     }
 
     let getSuc = async () => {
@@ -35,12 +41,15 @@ const StaffBarber = () => {
         setSucursal(list.data.rows)
     }
 
+    let filtBarber = (id) => {
+        setShow(barbers.filter((item)=> item.sucursalIdSucursal === id))
+    }
+
 
     useEffect(() => {
         getBarb()
         getSuc()
     },[])
-    
     
       
     return(
@@ -50,14 +59,14 @@ const StaffBarber = () => {
                 <p className={`text-white p-4 text-white  text-opacity-70`}>En "Estilo Clásico", no solo ofrecemos servicios de barbería, sino una experiencia excepcional que refleja nuestra pasión por el arte del cuidado capilar masculino. Nos complace presentar a nuestro talentoso equipo de barberos, quienes combinan habilidad, experiencia y dedicación para brindar un servicio excepcional a nuestros clientes.</p>
             </div>
             <div className="flex p-2">
-                <select className="select-staff" >
+                <select className="select-staff" onChange={() => filtBarber(event.target.value)} >
                     <option>SELECCIONE SUCURSAL</option>
                     {sucursal && sucursal.map(e => {
                         return <option key={e.idSucursal} value={e.idSucursal}>{e.nombre}</option>
                     })}
                 </select>
             </div>
-            <Carousel data={barbers}/>
+            <Carousel data={show}/>
         </div>
     )    
 }
