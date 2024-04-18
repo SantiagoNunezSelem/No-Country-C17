51c7dfabb5a6c5eed362aec0aea94fb159795e37
Santiago N.S. <gigogisantisd@gmail.com>
2024-04-18 00:44:43 -0300

    Update index.js

--- front/src/pages/reservar/[name]/index.js
+++ front/src/pages/reservar/[name]/index.js
@@ -3,6 +3,7 @@ import React, { useState } from "react"
 import NavBarCitas from '@/components/sitemaCitas/NavBarCitas'
 import BotonSiguiente from '@/components/sitemaCitas/BotonSiguiente';
 import CitaServios from "@/components/sitemaCitas/Servicios";
+import SeleccionarProfesional from "@/components/sitemaCitas/seleccionarProfesional-Seccion/SeleccionarProfesional"
 // import Servicios from '@/components/servicios/services';
 // import { servicios } from '@/lib/data';
 // import { motion } from 'framer-motion';
@@ -46,10 +47,11 @@ const Reservar = ({paramas}) => {
 
             <NavBarCitas numPaso={step} setNumPaso={setNumPaso} />
             <p>{step}</p>
-            {step === 1 && <CitaServios/> } {/* CODIGO CREADO POR SANTIAGO */}
+            {step === 1 && <CitaServios/> }
+            {step === 2 && <SeleccionarProfesional/>}
             
-            <div className='flex justify-end w-11/12'>
-                <BotonSiguiente next={handleNextButton} />
+            <div className="flex justify-end w-11/12">
+                <BotonSiguiente next={handleNextButton}/>
             </div>
         </div>
         )
