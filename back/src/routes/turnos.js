7df8f99ffcfc431b2f05c0ecd101292444213dc3
leoyasu <leandroyasutake@gmail.com>
2024-04-22 20:06:46 -0300

    agregando back turnos

--- back/src/routes/turnos.js
+++ back/src/routes/turnos.js
@@ -1,6 +1,12 @@
 const { Router } = require('express');
-
 const router = Router()
+
+//const {getAllTurnos, addTurno, deleteTurno, getByIdTurno} = require('./../controllers/sucursal')
+
+router.get('/' , getAllTurnos)
+router.post('/', addTurno )
+router.delete('/all', deleteTurno)
+router.get('/:id', getByIdTurno)
 
 
 module.exports = router
