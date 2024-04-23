f367812afdf92cf29e89b2becb637a0f471c36a9
Santiago N.S. <gigogisantisd@gmail.com>
2024-04-23 02:27:00 -0300

    navBar citas responsive

--- front/src/components/sitemaCitas/NavBarCitas.js
+++ front/src/components/sitemaCitas/NavBarCitas.js
@@ -7,27 +7,27 @@ function NavBarCitas( {numPaso,  setNumPaso} ) {
         <h3 className='text-center text-white text-2xl font-semibold p-4'>SISTEMA DE CITAS</h3> {/* damiian sola metio mano */}
         <div className='flex flex-row align-center justify-center w-3/4 m-auto'>
             
-            <div className={`container-paso-cita ${numPaso >= 1 ? "si" : "no"} ${numPaso == 1 ? "activo" : ""} `}>
+            <div className={`container-paso-cita ${numPaso >= 1 ? "si" : "no"} ${numPaso == 1 ? "activo" : "no-activo"} `}>
                 <div className='circle-icon' onClick={() => setNumPaso(1)}>1</div>
                 <p>Seleccionar servicios</p>
             </div>
 
-            <div className={`container-paso-cita ${numPaso >= 2 ? "si" : "no"} ${numPaso == 2 ? "activo" : ""} `}>
+            <div className={`container-paso-cita ${numPaso >= 2 ? "si" : "no"} ${numPaso == 2 ? "activo" : "no-activo"} `}>
                 <div className='circle-icon' onClick={() => setNumPaso(2)}>2</div>
                 <p>Seleccionar profesional</p>
             </div>
 
-            <div className={`container-paso-cita ${numPaso >= 3 ? "si" : "no"} ${numPaso == 3 ? "activo" : ""}`}>
+            <div className={`container-paso-cita ${numPaso >= 3 ? "si" : "no"} ${numPaso == 3 ? "activo" : "no-activo"}`}>
                 <div className='circle-icon' onClick={() => setNumPaso(3)}>3</div>
                 <p>Fijar fecha y hora</p>
             </div>
 
-            <div className={`container-paso-cita ${numPaso >= 4 ? "si" : "no"} ${numPaso == 4 ? "activo" : ""}`}>
+            <div className={`container-paso-cita ${numPaso >= 4 ? "si" : "no"} ${numPaso == 4 ? "activo" : "no-activo"}`}>
                 <div className='circle-icon' onClick={() => setNumPaso(4)}>4</div>
                 <p>Datos de contacto</p>
             </div>
 
-            <div className={`container-paso-cita ${numPaso >= 5 ? "si" : "no"} ${numPaso == 5 ? "activo" : ""}`}>
+            <div className={`container-paso-cita ${numPaso >= 5 ? "si" : "no"} ${numPaso == 5 ? "activo" : "no-activo"}`}>
                 <div className='circle-icon' onClick={() => setNumPaso(5)}>5</div>
                 <p>Resumen de la reserva</p>
             </div>
