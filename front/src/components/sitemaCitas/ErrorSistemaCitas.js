12c9c99d34372601bf31a52db60b6f6c7c4c803b
Santiago N.S. <gigogisantisd@gmail.com>
2024-04-23 23:06:59 -0300

    Update ErrorSistemaCitas.js

--- front/src/components/sitemaCitas/ErrorSistemaCitas.js
+++ front/src/components/sitemaCitas/ErrorSistemaCitas.js
@@ -7,9 +7,9 @@ function ErrorSistemaCitas({ mensaje, handleStateError }) {
     return (
         <>
             {state &&
-                <div className="fixed top-0 left-0 w-full h-full flex justify-center items-center z-50"
+                <div className="fixed top-0 left-0 w-full h-full flex justify-center items-center z-50 bg-black bg-opacity-50"
                     onClick={() => handleStateError({error:false,mensaje:""})}>
-                    <div className="bg-gray-800 bg-opacity-75 p-8 rounded-lg text-white text-center">
+                    <div className="bg-gray-900 p-8 rounded-lg text-white text-center">
                         <h2 className="text-lg font-bold mb-4">Error</h2>
                         <p>{mensaje}</p>
                     </div>
